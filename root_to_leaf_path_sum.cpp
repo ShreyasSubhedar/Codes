a79c680d2e8b121a21ade05790f4b2d953f8d906
ShreyasSubhedar <40920645+ShreyasSubhedar@users.noreply.github.com>
2019-12-01 12:38:52 +0530

    Tree DS

--- root_to_leaf_path_sum.cpp
+++ root_to_leaf_path_sum.cpp
@@ -3,7 +3,21 @@
 using namespace std;
 /* A binary tree node has data, pointer to left child
    and a pointer to right child */
-   
+   /*       6                               
+         /   \                          
+        3     5                      
+      /   \     \
+     2    5      4             
+        /  \                        
+       7    4                 
+            
+  There are 4 leaves, hence 4 root to leaf paths:
+  Path                      Number
+  6->3->2                   600+30+2=632
+  6->3->5->7                6000+300+50+7=6357
+  6->3->5->4                6000+300+50+4=6354
+  6->5>4                    600+50+4=654   
+Answer = 632 + 6357 + 6354 + 654 = 13997 */
 struct Node
 {
 	int data;
